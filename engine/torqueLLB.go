50728590dc2618c4dda26c558b5e96c05a8c8300
aleste2 <victor@usal.es>
2018-05-31 17:44:36 +0200

    Create torqueLLB.go
    Torque functions for LLB for different models

---
+++ engine/torqueLLB.go
@@ -0,0 +1,99 @@
+package engine
+
+import (
+	"github.com/mumax/3/cuda"
+	"github.com/mumax/3/data"
+	"github.com/mumax/3/util"
+)
+
+// For LLB the same functions as for LLG
+
+// Sets dst to the current total LLBtorque
+func SetTorqueLLB(dst *data.Slice,hth1 *data.Slice,hth2 *data.Slice) {
+	SetLLBTorque(dst,hth1,hth2)
+	AddSTTorque(dst)
+	FreezeSpins(dst)
+}
+
+// Sets dst to the current Landau-Lifshitz-Bloch torque
+func SetLLBTorque(dst *data.Slice,hth1 *data.Slice,hth2 *data.Slice) {
+	SetEffectiveField(dst) // calc and store B_eff
+	alpha := Alpha.MSlice()
+	defer alpha.Recycle()
+	TCurie := TCurie.MSlice()
+	defer TCurie.Recycle()
+	Msat := Msat.MSlice()
+	defer Msat.Recycle()
+	Temp := Temp.MSlice()
+	defer Temp.Recycle()
+
+  cuda.Zero(hth1)       
+  B_therm.AddTo(hth1)
+  cuda.Zero(hth2)       
+  B_therm.AddTo(hth2)
+	if Precess {
+		cuda.LLBTorque(dst, M.Buffer(), dst, Temp,alpha,TCurie,Msat,hth1,hth2) // overwrite dst with torque
+	} else {
+		cuda.LLNoPrecess(dst, M.Buffer(), dst)
+	}
+}
+
+///////////////// For LLBJH the same previous two functions
+
+// Sets dst to the current total LLBtorque
+func SetTorqueLLBJH(dst *data.Slice,hth1 *data.Slice,hth2 *data.Slice) {
+	SetLLBTorqueJH(dst,hth1,hth2)
+	AddSTTorque(dst)
+	FreezeSpins(dst)
+}
+
+// Sets dst to the current Landau-Lifshitz-Bloch torque
+func SetLLBTorqueJH(dst *data.Slice,hth1 *data.Slice,hth2 *data.Slice) {
+	SetEffectiveField(dst) // calc and store B_eff
+	alpha := Alpha.MSlice()
+	defer alpha.Recycle()
+	TCurie := TCurie.MSlice()
+	defer TCurie.Recycle()
+	Msat := Msat.MSlice()
+	defer Msat.Recycle()
+
+  cuda.Zero(hth1)       
+  B_therm.AddTo(hth1)
+  cuda.Zero(hth2)       
+  B_therm.AddTo(hth2)
+	if Precess {
+		cuda.LLBTorqueJH(dst, M.Buffer(), dst, TempJH.temp,alpha,TCurie,Msat,hth1,hth2) // overwrite dst with torque
+	} else {
+		cuda.LLNoPrecess(dst, M.Buffer(), dst)
+	}
+}
+
+///////////////// For 3T model the same previous two functions
+
+// Sets dst to the current total LLBtorque
+func SetTorqueLLB3T(dst *data.Slice,hth1 *data.Slice,hth2 *data.Slice) {
+	SetLLBTorque3T(dst,hth1,hth2)
+	AddSTTorque(dst)
+	FreezeSpins(dst)
+}
+
+// Sets dst to the current Landau-Lifshitz-Bloch torque
+func SetLLBTorque3T(dst *data.Slice,hth1 *data.Slice,hth2 *data.Slice) {
+	SetEffectiveField(dst) // calc and store B_eff
+	alpha := Alpha.MSlice()
+	defer alpha.Recycle()
+	TCurie := TCurie.MSlice()
+	defer TCurie.Recycle()
+	Msat := Msat.MSlice()
+	defer Msat.Recycle()
+
+  cuda.Zero(hth1)       
+  B_therm.AddTo(hth1)
+  cuda.Zero(hth2)       
+  B_therm.AddTo(hth2)
+	if Precess {
+		cuda.LLBTorqueJH(dst, M.Buffer(), dst, Ts.temp,alpha,TCurie,Msat,hth1,hth2) // overwrite dst with torque
+	} else {
+		cuda.LLNoPrecess(dst, M.Buffer(), dst)
+	}
+}
