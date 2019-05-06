228d34fa53561ea59e4740fb9036a4f839e04fea
aleste2 <victor@usal.es>
2019-05-06 17:33:47 +0200

    2T model

--- engine/torqueLLB.go
+++ engine/torqueLLB.go
@@ -96,3 +96,33 @@ func SetLLBTorque3T(dst *data.Slice,hth1 *data.Slice,hth2 *data.Slice) {
 		cuda.LLNoPrecess(dst, M.Buffer(), dst)
 	}
 }
+
+///////////////// For 2T model the same previous two functions
+
+// Sets dst to the current total LLBtorque
+func SetTorqueLLB2T(dst *data.Slice,hth1 *data.Slice,hth2 *data.Slice) {
+	SetLLBTorque2T(dst,hth1,hth2)
+	AddSTTorque(dst)
+	FreezeSpins(dst)
+}
+
+// Sets dst to the current Landau-Lifshitz-Bloch torque
+func SetLLBTorque2T(dst *data.Slice,hth1 *data.Slice,hth2 *data.Slice) {
+	SetEffectiveField(dst) // calc and store B_eff
+	alpha := Alpha.MSlice()
+	defer alpha.Recycle()
+	TCurie := TCurie.MSlice()
+	defer TCurie.Recycle()
+	Msat := Msat.MSlice()
+	defer Msat.Recycle()
+
+  cuda.Zero(hth1)       
+  B_therm.LLBAddTo(hth1)
+  cuda.Zero(hth2)       
+  B_therm.LLBAddTo(hth2)
+	if Precess {
+		cuda.LLBTorqueJH(dst, M.Buffer(), dst, Te.temp,alpha,TCurie,Msat,hth1,hth2) // overwrite dst with torque
+	} else {
+		cuda.LLNoPrecess(dst, M.Buffer(), dst)
+	}
+}
