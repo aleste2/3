b07db83156244311688216d58c57377b989ddba7
aleste2 <victor@usal.es>
2019-06-10 17:55:42 +0200

    Langevin Ms(T)

---
+++ cuda/ext_LLB_functions.h
@@ -0,0 +1,19 @@
+#include "float3.h"
+#include "math.h"
+
+// Langevin function
+inline __device__ float Brillouin(float x,float J)
+{
+  return ( (2.0f*J+1.0f)/(2.0f*J)/tanh((2.0f*J+1.0f)/(2.0f*J)*x) - 1.0f/(2.0f*J)/tanh(x/(2.0f*J)));
+}
+
+// Langevin derivative function
+inline __device__ float DBrillouin(float x,float J)
+{
+ // if (x<6){
+  return ( pow(sinh(x/(2.0f*J)),-2.0f) /(4.0f*J*J) - (1.0f+2.0f*J)*(1.0f+2.0f*J)* pow(sinh((1.0f+2.0f*J)/(2.0f*J)*x),-2.0f) /(4.0f*J*J));
+ // }else{
+ // return(0.000025073);
+ // }
+}
+
