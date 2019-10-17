0edb077cb0239b3b1bdec4d5dd5bd8e2ad322975
aleste2 <victor@usal.es>
2019-10-17 18:48:56 +0200

    Separated B12-21

--- cuda/ext_AF_AddExchangeCell.cu
+++ cuda/ext_AF_AddExchangeCell.cu
@@ -10,7 +10,8 @@ addExchangeAFCell(float* __restrict__ dst1x, float* __restrict__ dst1y, float* _
 	float* __restrict__ m2x, float* __restrict__ m2y, float* __restrict__ m2z,
 	float* __restrict__  Ms1_, float  Ms1_mul,
 	float* __restrict__  Ms2_, float  Ms2_mul,
-	float* __restrict__  Bex_, float  Bex_mul,
+	float* __restrict__  Bex12_, float  Bex12_mul,
+    	float* __restrict__  Bex21_, float  Bex21_mul,
 	int N) {
 
     int i =  ( blockIdx.y*gridDim.x + blockIdx.x ) * blockDim.x + threadIdx.x;
@@ -18,12 +19,13 @@ addExchangeAFCell(float* __restrict__ dst1x, float* __restrict__ dst1y, float* _
 
         float invMs1 = inv_Msat(Ms1_, Ms1_mul, i);
         float invMs2 = inv_Msat(Ms2_, Ms2_mul, i);
-        float bex = amul(Bex_, Bex_mul, i);
-        dst1x[i] += invMs1*bex*m2x[i];
-        dst1y[i] += invMs1*bex*m2y[i];
-        dst1z[i] += invMs1*bex*m2z[i];
-        dst2x[i] += invMs2*bex*m1x[i];
-        dst2y[i] += invMs2*bex*m1y[i];
-        dst2z[i] += invMs2*bex*m1z[i];
+        float bex12 = amul(Bex12_, Bex12_mul, i);
+        float bex21 = amul(Bex21_, Bex21_mul, i);
+        dst1x[i] += invMs1*bex12*m2x[i];
+        dst1y[i] += invMs1*bex12*m2y[i];
+        dst1z[i] += invMs1*bex12*m2z[i];
+        dst2x[i] += invMs2*bex21*m1x[i];
+        dst2y[i] += invMs2*bex21*m1y[i];
+        dst2z[i] += invMs2*bex21*m1z[i];
     }
 }
